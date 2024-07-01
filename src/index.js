3f994e4fed2827e615ac65671e6f705cf7a32a84
Eugena <eugena.h12345@gmail.com>
2024-07-01 22:14:00 +0300

    add state and eventlistener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import './styles.scss';
 import 'bootstrap';
+import { string } from 'yup';
 
-console.log('Hello World!');
+const inputElement = document.querySelector('#url-input');
+
+const state = {
+  urlColl: [],
+};
+const isDubble = (rssUrl) => {
+  console.log(state.urlColl.includes(rssUrl));
+  console.log(state.urlColl);
+
+  if (state.urlColl.includes(rssUrl)) {
+    return true;
+  }
+  return false;
+};
+
+const form = document.querySelector('form');
+
+// console.log(form);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const rssUrl = form.elements[0].value;
+  const schema = string().url().nullable();
+  schema.isValid(rssUrl)
+    .then((data) => {
+      const isDubbled = isDubble(rssUrl);
+      console.log(isDubbled);
+      if (data && !isDubbled) {
+        state.urlColl.push(rssUrl);
+        inputElement.classList.remove('is-invalid');
+        form.reset();
+        console.log(state);
+      } else {
+        const divInput = document.querySelector('.mx-auto');
+        const addingRssResult = document.createElement('p');
+        addingRssResult.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success');
+        addingRssResult.textContent = 'RSS НЕЕЕЕЕ загружен';
+        divInput.append(addingRssResult);
+        console.log('не содержит валидный рсс');
+        inputElement.classList.add('is-invalid');
+      }
+    });
+});
